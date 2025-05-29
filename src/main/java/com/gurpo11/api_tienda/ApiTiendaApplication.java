7a29a0eb567c02c312c19f6d5e837597d57f9407
Nathan Bernal <jo.bernalv@duocuc.cl>
2025-05-29 12:58:51 -0400

    Feat(main): Config project

---
+++ src/main/java/com/gurpo11/api_tienda/ApiTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.gurpo11.api_tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gurpo11/api_tienda/ApiTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gurpo11.api_tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
